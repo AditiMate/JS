11f834ce93b8c3fb2f4b3b89784c1a4322887f5f
Aditi Mate <aditimate0075@gmail.com>
2024-03-11 14:38:16 +0530

    Add files via upload

---
+++ Positive,neg,zero.js
@@ -0,0 +1,13 @@
+// program that checks if the number is positive, negative or zero
+
+let number = 0;
+
+if(number > 0){
+    console.log("The number is positive");
+}
+else if (number == 0){
+    console.log("The number is zero");
+}
+else{
+    console.log("The number is negative");
+}
