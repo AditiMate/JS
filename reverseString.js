72c4fb1afdd90f8bd7fb69247351582493efa4d1
Aditi Mate <aditimate0075@gmail.com>
2024-03-14 08:21:04 +0530

    Reverse a string

---
+++ reverseString.js
@@ -0,0 +1,11 @@
+// program to reverse a string
+
+function reverseString(str){
+    let newString = "";
+    for(let i = str.length - 1 ; i >= 0 ; i--){
+        newString += str[i]
+    }
+    return newString
+}
+
+console.log(reverseString("Hello world"));
